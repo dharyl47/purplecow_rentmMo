d19a40592bc444fc273edcabc86153d26b7f704b
Dharyl Bajenting <dharyljersonbajenting11@gmail.com>
2023-11-07 11:10:31 +0800

    Zustand Setup useUser

---
+++ app/hooks/useUser.ts
@@ -0,0 +1,50 @@
+// useUser.ts
+
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+interface IUser {
+  user: User;
+  location: string;
+}
+
+type User = {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string | '';
+  phoneNumber: string;
+  city: string;
+  state: string;
+  country: string;
+  __v: number;
+  createdAt: Date;
+  isHost: boolean;
+  profession?: string;
+  language?: string;
+  isVerified: boolean;
+  isLicensed: boolean;
+};
+
+type UserState = {
+  user: IUser | null;
+  setUser: (user: IUser | null) => void;
+};
+
+export const useUser = create(
+  persist<UserState>(
+    (set) => ({
+      user: null,
+      setUser: (user: IUser | null) => set({ user }),
+    }),
+    {
+      name: 'user-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
+
+
+
+  
