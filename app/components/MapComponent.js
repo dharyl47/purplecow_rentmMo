da705225304e6ac8b56502f9dab561590f778d2a
johnrey <johnrey@purplecowph.com>
2023-12-18 18:08:58 +0800

    Cleaning code and Delete all Comment codes

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -1,131 +1,4 @@
 "use client";
-// import React, { useState } from "react";
-// import ReactMapGL, { Marker, Popup } from "react-map-gl";
-// import 'mapbox-gl/dist/mapbox-gl.css';
-
-// const MapComponent = () => {
-//   const [viewport, setViewport] = useState({
-//     width: "100%",
-//     height: "400px",
-//     latitude: 7.1907,
-//     longitude: 125.4553,
-//     zoom: 12,
-//   });
-
-//   const [selectedLocation, setSelectedLocation] = useState({
-//     lng: 125.4553,
-//     lat: 7.1907,
-//   });
-
-//   const handleMapClick = (event) => {
-//     if (event.lngLat) {
-//       const { lng, lat } = event.lngLat;
-//       // Update selectedLocation to match the clicked coordinates
-//       setSelectedLocation({ lat, lng });
-//     } else {
-//       console.error("Invalid event object or lngLat format:", event);
-//     }
-//   };
-
-//   return (
-    
-//       <ReactMapGL
-//         {...viewport}
-//         mapboxAccessToken="pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2w5NTluMDh2MXQ3YTNucW16cG9tbGU3dyJ9.z96hyUi9vkmIJDdBB6WjxA"
-//         mapStyle="mapbox://styles/mapbox/streets-v11"
-//         onViewportChange={(viewport) => setViewport(viewport)}
-//         onClick={handleMapClick}
-//       >
-//         <Marker
-//           latitude={selectedLocation.lat} // Use selectedLocation's lat
-//           longitude={selectedLocation.lng} // Use selectedLocation's lng
-//         >
-//           {/* Your marker UI or component */}
-//         </Marker>
-//       </ReactMapGL>
-    
-//   );
-// };
-
-// export default MapComponent;
-
-
-// import React, { useState, useEffect } from 'react';
-// import ReactMapGL, { Marker } from "react-map-gl";
-// import 'mapbox-gl/dist/mapbox-gl.css';
-// import { RouteModule } from 'next/dist/server/future/route-modules/route-module';
-
-// const MapComponent = ({ handleChangeLat, handleChangeLon }) => {
-//   const defaultLocation = { lat: 7.1907, lng: 125.4553 };
-//   // Local state for viewport
-//   const [viewPort, setViewport] = useState({
-//     latitude: defaultLocation.lat,
-//     longitude: defaultLocation.lng,
-//     zoom: 12,
-//   });
-
-//   const [selectedLocation, setSelectedLocation] = useState({
-//     lat: 0,
-//     lng: 0,
-//   });
-
-//   const handleMapClick = (event) => {
-//     console.log("Map Clicked", event);
-//     if (event.lngLat) {
-//       const { lng, lat } = event.lngLat;
-//       console.log("Clicked Coordinates: lat =", lat, "lng =", lng);
-      
-//       setSelectedLocation({ lat, lng });
-//       setViewport({
-//         ...viewPort,
-//         latitude: lat,
-//         longitude: lng,
-//       });
-//      handleChangeLon('lon', lng);
-//     } else {
-//       console.error("Invalid event object or lngLat format:", event);
-//     }
-//   };
-//     // Log coordinates when selectedLocation changes
-//     useEffect(() => {   
-//        handleChangeLat('lat', selectedLocation.lat);
-//     }, [selectedLocation]);
-
-//   return (
-//     <ReactMapGL
-//       {...viewPort}
-//       mapboxAccessToken="pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2w5NTluMDh2MXQ3YTNucW16cG9tbGU3dyJ9.z96hyUi9vkmIJDdBB6WjxA"
-//       mapStyle="mapbox://styles/mapbox/streets-v11"
-//       width='100%'
-//       height='100%'
-//       onViewportChange={(viewPort) => setViewport(viewPort)}
-//       onClick={handleMapClick}
-//       transitionDuration='100'
-//     >
-//       {selectedLocation ?(
-//         <>
-//       <Marker
-//         latitude={selectedLocation?.lat} // Use selectedLocation's lat, default to 0 if null
-//         longitude={selectedLocation?.lng} // Use selectedLocation's lng, default to 0 if null
-//         offsetLeft={-3.5 * viewPort.zoom}
-//         offsetTop={-7 * viewPort.zoom}
-//       >
-//       {/* <Room>
-//         style={{
-//           fontSize: 7 * viewPort.zoom,
-//           color: "tomato",
-//           cursor: "pointer",
-//         }}
-//       </Room> */}
-//       </Marker>
-//       </>
-//       ) :null}
-//     </ReactMapGL>
-//   );
-// };
-
-
-// export default MapComponent;
 
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker } from "react-map-gl";
@@ -133,7 +6,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { RouteModule } from 'next/dist/server/future/route-modules/route-module';
 
 
-const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, handleChangeCity, handleChangeCountry, handleChangeZipCode, handleChangeCounty, }) => {
+const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon }) => {
   
   const defaultLocation = { lat: 7.1907, lng: 125.4553 };
   // Local state for viewport
@@ -148,28 +21,15 @@ const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, ha
     const [countryState, setCountryState] = useState();
     const [countyState, setCountyState] = useState();
     const [streetAddress, setStreetAddress] = useState();
-  
-  const [selectedLocation, setSelectedLocation] = useState(null);
-  // const [selectedLocation, setSelectedLocation] = useState({
-  //   lat: 0,
-  //   lng: 0,
-  // });
+    const [selectedLocation, setSelectedLocation] = useState(null);
 
   const handleMapClick = async (event) => {
-    // console.log("Map Clicked", event);
     if (event.lngLat) {
       const { lng, lat } = event.lngLat;
-      // console.log("Clicked Coordinates: lat =", lat, "lng =", lng);
-      
-      // setSelectedLocation({ lat, lng });
-      
-    //  handleChangeLon('lon', lng);
 
      try {
       // Fetch city and country information using Mapbox Geocoding API
       const response = await fetch(
-        // "https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2w5NTluMDh2MXQ3YTNucW16cG9tbGU3dyJ9.z96hyUi9vkmIJDdBB6WjxA"
-        // "https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2xwMmQ2ZGRiMHd1ZTJrbzVmOWtqczV3eSJ9.MC9rvcpLcK3IekyFDcvDig"
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2w5NTluMDh2MXQ3YTNucW16cG9tbGU3dyJ9.z96hyUi9vkmIJDdBB6WjxA`
       );
 
@@ -182,9 +42,9 @@ const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, ha
         const county = place.context.find((item) => item.id.startsWith('region'))?.text;
         const streetAdd = place.text;
         setStreetAddress(streetAdd)
-        
+
           console.log('LOCATION DETAILS');
-          console.log('Lplace234', streetAdd);
+          console.log('Street:', streetAdd);
           console.log('Lat:', lat);
           console.log('Lon:', lng);
           console.log('City:', city);
@@ -198,19 +58,16 @@ const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, ha
             latitude: lat,
             longitude: lng,
           });
-        // Update city and country in the form fields
-        // if (city, country) {
           // Update your form fields (assuming you have functions like handleChangeCity and handleChangeCountry)
 
           setCityState(city);
           setCountryState(country);
           setZipState(zipCode);
           setCountyState(county);
-
+          
           handleChangeLat('lat', lat);
           handleChangeLon('lon', lng);
-         
-        // }
+
         // Update street information
       } else {
         console.error('Failed to fetch data from Mapbox Geocoding API');
@@ -241,7 +98,6 @@ const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, ha
     <ReactMapGL
       // {...viewPort}
       mapboxAccessToken="pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2w5NTluMDh2MXQ3YTNucW16cG9tbGU3dyJ9.z96hyUi9vkmIJDdBB6WjxA"
-      // mapboxAccessToken="pk.eyJ1IjoiZGhhcnlsOTciLCJhIjoiY2xwMmQ2ZGRiMHd1ZTJrbzVmOWtqczV3eSJ9.MC9rvcpLcK3IekyFDcvDig"
       mapStyle="mapbox://styles/mapbox/streets-v11"
       width='100%'
       height='100%'
@@ -254,11 +110,7 @@ const MapComponent = ({ handleChangeUpdate, handleChangeLat, handleChangeLon, ha
         <Marker
           latitude={selectedLocation.lat}
           longitude={selectedLocation.lng}
-          // offsetLeft={-3.5 * viewPort.zoom}
-          // offsetTop={-7 * viewPort.zoom}
         >
-          {/* Marker content here */}
-          {/* <div style={{ color: 'red', fontSize: '20px' }}>X</div> */}
         </Marker>
         </>
       ) : null}
--- app/forms/PersonalInfoForm.tsx
+++ app/forms/PersonalInfoForm.tsx
@@ -1,235 +1,3 @@
-// import React, { useState } from "react";
-// import {
-//   TextField,
-//   ThemeProvider,
-//   Modal,
-//   Button,
-//   Backdrop
-// } from "@mui/material";
-// import { ICar } from "../types/types";
-// import { theme } from "./themes/themes";
-// import MapComponent from "../components/MapComponent"; // Import the MapComponent
-
-// type Props = {
-//   handleChange: (e: any) => void;
-//   personalInfo: ICar;
-// };
-
-// const PersonalInfoForm = ({ handleChange, personalInfo }: Props) => {
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [isHovered, setIsHovered] = useState(false);
-//   const handleModalOpen = () => {
-//     setModalOpen(true);
-//   };
-
-//   const handleModalClose = () => {
-//     setModalOpen(false);
-//   };
-
-//     const handleCloseButtonClick = () => {
-//       setModalOpen(false);
-//     };
-// 	  const handleMouseEnter = () => {
-//       setIsHovered(true);
-//     };
-
-//     const handleMouseLeave = () => {
-//       setIsHovered(false);
-//     };
-//  const buttonBackgroundColor = isHovered
-//    ? theme.palette.secondary.main // Hover background color
-//    : theme.palette.primary.main;
-//   return (
-//     <>
-//       <ThemeProvider theme={theme}>
-//         <div>
-//           <div className=" flex lg:flex-row items-center lg:gap-8 flex-col gap-0 lg:mt-5 mt-0">
-//             <div className=" flex flex-col lg:mt-0 mt-5 w-full">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Mobile Number
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="mobileNumber"
-//                 id="mobileNumber"
-//                 value={personalInfo.mobileNumber}
-//                 type="number"
-//                 placeholder="09XXXXXXXXX"
-//                 required
-//               />
-//             </div>
-//             <div className="flex flex-col lg:mt-0 mt-5 w-full">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Email Address
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="email"
-//                 id="email"
-//                 value={personalInfo.email}
-//                 type="email"
-//                 placeholder="rentmo@gmail.com"
-//                 required
-//               />
-//             </div>
-//           </div>
-//           <div className="lg:mt-5 mt-0 flex lg:flex-row items-center lg:gap-8 flex-col gap-0">
-//             <div className="flex flex-col lg:mt-0 mt-5 w-full">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Street Address
-//               </label>
-//               <p
-//                 className="mb-3 text-sm leading-none text-dark900"
-//                 style={{ color: "blue", cursor: "pointer" }}
-//                 onClick={handleModalOpen}
-//               >
-//                 Pin on Location
-//               </p>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="street"
-//                 id="street"
-//                 value={personalInfo.street}
-//                 type="text"
-//                 placeholder="Unit#/Street/Barangay"
-//                 required
-//               />
-//             </div>
-//             <div className="flex flex-col lg:mt-0 mt-5 w-full">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 City
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="city"
-//                 id="city"
-//                 value={personalInfo.city}
-//                 type="text"
-//                 placeholder="Enter City"
-//                 required
-//               />
-//             </div>
-//           </div>
-//           <div className="lg:mt-5 mt-0 flex lg:flex-row items-center lg:gap-8 flex-col gap-0">
-//             <div className="flex flex-col w-full lg:mt-0 mt-5">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Province
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="state"
-//                 id="state"
-//                 value={personalInfo.state}
-//                 type="text"
-//                 placeholder="Enter Province"
-//                 required
-//               />
-//             </div>
-//             <div className="flex flex-col w-full lg:mt-0 mt-5">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Country
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="country"
-//                 id="country"
-//                 value={personalInfo.country}
-//                 type="text"
-//                 placeholder="Enter Country"
-//                 required
-//               />
-//             </div>
-//             <div className="flex flex-col lg:w-1/2 w-full lg:my-0 mt-5 mb-10">
-//               <label className="mb-3 text-sm leading-none text-dark900">
-//                 Zip Code
-//               </label>
-//               <TextField
-//                 variant="outlined"
-//                 size="small"
-//                 onChange={handleChange}
-//                 name="zipCode"
-//                 id="zipCode"
-//                 value={personalInfo.zipCode}
-//                 type="number"
-//                 placeholder="Zip Code"
-//                 required
-//               />
-//             </div>
-//           </div>
-//           <Modal
-//             open={modalOpen}
-//             onClose={handleModalClose}
-//             aria-labelledby="map-modal"
-//             aria-describedby="map-modal-description"
-//             closeAfterTransition
-//             BackdropComponent={Backdrop}
-//             BackdropProps={{
-//               timeout: 500,
-//             }}
-//           >
-//             <div
-//               style={{
-//                 display: "flex",
-//                 justifyContent: "center",
-//                 alignItems: "center",
-//                 width: "100%", // Set width to occupy the entire modal
-//                 height: "100%", // Set height to occupy the entire modal
-//               }}
-//             >
-//               <div
-//                 style={{
-//                   position: "relative",
-//                   width: "80%", // Adjust the width of the map container
-//                   height: "80%", // Adjust the height of the map container
-//                   backgroundColor: "white",
-//                   padding: "20px",
-//                   borderRadius: "8px",
-//                 }}
-//               >
-//                 <Button
-//                   onClick={handleCloseButtonClick}
-//                   onMouseEnter={handleMouseEnter}
-//                   onMouseLeave={handleMouseLeave}
-//                   style={{
-//                     position: "absolute",
-//                     top: "10px",
-//                     right: "10px",
-//                     zIndex: 9999,
-//                     borderRadius: "50%", // Rounded corners
-//                     width: "40px", // Larger width
-//                     height: "40px", // Larger height
-//                     minWidth: "unset", // Remove minimum width
-//                     backgroundColor: buttonBackgroundColor, // Set background color dynamically
-//                     transition: "background-color 0.3s ease", // Smooth transition
-//                   }}
-//                   variant="contained"
-//                 >
-//                   X
-//                 </Button>
-//                 <MapComponent />
-//               </div>
-//             </div>
-//           </Modal>
-//         </div>
-//       </ThemeProvider>
-//     </>
-//   );
-// };
-
-// export default PersonalInfoForm;
-
 import React, { useState, useEffect, useRef } from "react";
 import {
   TextField,
@@ -262,7 +30,6 @@ type Props = {
 // Added by Leo
 const apiKey = process.env.APP_GMAP_API_KEY as string;
 const mapApiJs = "https://maps.googleapis.com/maps/api/js";
-// const geocodeJson = "https://maps.googleapis.com/maps/api/geocode/json";
 interface Address {
   street1: string;
   street2: string;
@@ -301,40 +68,7 @@ const PersonalInfoForm = ({
       },
     });
   };
-  const handleChangeCity = (name: any, value: any) => {
-    handleChange({
-      target: {
-        name: name,
-        value: value,
-      },
-    });
-  };
-  const handleChangeCountry = (name: any, value: any) => {
-    handleChange({
-      target: {
-        name: name,
-        value: value,
-      },
-    });
-  };
-  const handleChangeZipCode = (name: any, value: any) => {
-    handleChange({
-      target: {
-        name: name,
-        value: value,
-      },
-    });
-  };
-  const handleChangeCounty = (name: any, value: any) => {
-    handleChange({
-      target: {
-        name: name,
-        value: value,
-      },
-    });
-  };
   
-
   const handleModalOpen = () => {
     setModalOpen(true);
   };
@@ -397,25 +131,10 @@ const PersonalInfoForm = ({
     setLatX(latitude);
     setLonY(longitude);
 
-    // let zipCode = "";
-    // // Extracting the zip code from address components
-    // for (const component of addressComponents) {
-    //   if (component.types.includes("postal_code")) {
-    //     zipCode = component.long_name;
-    //     break;
-    //   }
-    // }
-
     let isAutoFill = true; // Assuming this change is due to auto-fill
     setAddress(extractAddress(place));
     // Trigger changes only if the event is not due to auto-fill
   };
-
-
-// useEffect(() => {
-//   // Use the new handleUpdateState function to update the state immediately
-//   handleChangeUpdate(latX+"", lonY+"", city, state, country, street1, street2, zipCode);
-// }, [latX, lonY, city, country, state, street1, street2, zipCode, personalInfo]);
 
     useEffect(() => {
       let isMounted = true;
@@ -474,17 +193,6 @@ const PersonalInfoForm = ({
       lon: "",
       country: "",
       plain: () => "",
-      // plain() {
-      //   const street1 = this.street1 ? this.street1 + ", " : "";
-      //   const street2 = this.street2 ? this.street2 + ", " : "";
-      //   const city = this.city ? this.city + ", " : "";
-      //   const zipCode = this.zipCode ? this.zipCode + ", " : "";
-      //   const state = this.state ? this.state + ", " : "";
-      //   const lat = this.lat ? "Lat: " + this.lat + ", " : "";
-      //   const lon = this.lon ? "Lon: " + this.lon + ", " : "";
-      //   const country = this.country ? this.country + ", " : "";
-      //   return street1 + street2 + city + zipCode + state + lat + lon + country;
-      // },
     };
 
     if (!Array.isArray(place?.address_components)) {
@@ -527,12 +235,6 @@ const PersonalInfoForm = ({
         }
       }
     );
-
-    // Get latitude and longitude
-    // if (place.geometry && place.geometry.location) {
-    //   address.lat = place.geometry.location.lat().toString();
-    //   address.lon = place.geometry.location.lng().toString();
-    // }
 
     return address;
   };
@@ -595,7 +297,7 @@ const PersonalInfoForm = ({
                 name="street"
                 id="street"
                 defaultValue={personalInfo.street1}
-                //value={personalInfo.street1 || personalInfo.street2}
+                // value={personalInfo.street1 || personalInfo.street2}
                 type="text"
                 placeholder="Unit#/Street/Barangay"
                 inputRef={searchInput}
@@ -746,18 +448,12 @@ const PersonalInfoForm = ({
                 <MapComponent
                   handleChangeLat={handleChangeLat}
                   handleChangeLon={handleChangeLon}
-                  handleChangeCity={handleChangeCity}
-                  handleChangeCountry={handleChangeCountry}
-                  handleChangeZipCode={handleChangeZipCode}
-                  handleChangeCounty={handleChangeCounty}
                   handleChangeUpdate={handleChangeUpdate}
                 />
               </div>
             </div>
           </Modal>
         </div>
-        {/* Display or use selectedLocation in your form */}
-        {/* <p>Selected Location: {`Lat: ${selectedLocation.lat}, Lng: ${selectedLocation.lng}`}</p> */}
       </ThemeProvider>
 
     </>
