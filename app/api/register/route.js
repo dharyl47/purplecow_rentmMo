de6eb73a9c08bc339b3ddcb230b02151f5877782
Jubet <jubet.sode.5@gmail.com>
2024-03-12 09:21:07 +0800

    fix vercel updates

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,42 +1,48 @@
-// Third Party Components
-import bcrypt from 'bcryptjs';
-import prisma from "@/prisma"
-import { NextResponse } from "next/server";
+// // Third Party Components
+// import bcrypt from "bcryptjs";
+// import prisma from "@/prisma";
+// import { NextResponse } from "next/server";
 
-// Database Connect
-import { connectToDatabase } from "@/helpers/ServerHelpers"
+// // Database Connect
+// import { connectToDatabase } from "@/helpers/ServerHelpers";
 
-export async function POST(request) {
-    try {
-        let requestData = await request.json();
-        const email = requestData.email;
-        const password = requestData.password; 
-  
-        await connectToDatabase();
-  
-        // Check if the email already exists
-        const existingUser = await prisma.users.findFirst({
-            where: {
-              email: email
-            }
-          });
+// export async function POST(request) {
+//   try {
+//     let requestData = await request.json();
+//     const email = requestData.email;
+//     const password = requestData.password;
 
-        if (existingUser) {
-            return NextResponse.json({ message: "Email already exists" }, { status: 400 });
-        }
-  
-        const hashedPassword = await bcrypt.hash(password, 10);
-  
-        requestData.password = hashedPassword
+//     await connectToDatabase();
 
-        // Create new user if email doesn't exist
-       const newUser = await prisma.users.create({
-            data: requestData
-        }); 
-  
-        return NextResponse.json({ message: "User Created" }, { status: 200 });
-    } catch (error) {
-        console.error("Error creating user:", error);
-        return NextResponse.json({ message: "Something went wrong. Please try again." }, { status: 500 });
-    }
-}
+//     // Check if the email already exists
+//     const existingUser = await prisma.users.findFirst({
+//       where: {
+//         email: email
+//       }
+//     });
+
+//     if (existingUser) {
+//       return NextResponse.json(
+//         { message: "Email already exists" },
+//         { status: 400 }
+//       );
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     requestData.password = hashedPassword;
+
+//     // Create new user if email doesn't exist
+//     const newUser = await prisma.users.create({
+//       data: requestData
+//     });
+
+//     return NextResponse.json({ message: "User Created" }, { status: 200 });
+//   } catch (error) {
+//     console.error("Error creating user:", error);
+//     return NextResponse.json(
+//       { message: "Something went wrong. Please try again." },
+//       { status: 500 }
+//     );
+//   }
+// }
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs'; 
+import bcrypt from "bcryptjs";
 
 // Next Connect
 import { NextResponse } from "next/server";
@@ -9,62 +9,73 @@ import connectMongoDB from "@/lib/mongodb";
 // Model
 import UserSchema from "@/lib/models/user.model";
 
-
 export async function POST(request) {
   try {
-      let requestData = await request.json();
-      const email = requestData.email;
-      const password = requestData.password; 
+    let requestData = await request.json();
+    const email = requestData.email;
+    const password = requestData.password;
 
-      // Connect to MongoDB 
-      await connectMongoDB();
+    // Connect to MongoDB
+    await connectMongoDB();
 
-      // Check if the email already exists
-      const existingUser = await UserSchema.findOne({ email });
-      if (existingUser) {
-          return NextResponse.json({ message: "Email already exists" }, { status: 400 });
-      }
+    // Check if the email already exists
+    const existingUser = await UserSchema.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "Email already exists" },
+        { status: 400 }
+      );
+    }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      requestData.password = hashedPassword
+    requestData.password = hashedPassword;
 
-      // Create new user if email doesn't exist
-      await UserSchema.create(requestData);
+    // Create new user if email doesn't exist
+    await UserSchema.create(requestData);
 
-      return NextResponse.json({ message: "User Created" }, { status: 200 });
+    return NextResponse.json({ message: "User Created" }, { status: 200 });
   } catch (error) {
-      console.error("Error creating user:", error);
-      return NextResponse.json({ message: "Something went wrong. Please try again." }, { status: 500 });
+    console.error("Error creating user:", error);
+    return NextResponse.json(
+      { message: "Something went wrong. Please try again." },
+      { status: 500 }
+    );
   }
 }
 
 export async function GET(request, { query }) {
-    await connectMongoDB();
-     const queryParams = new URL(request.url).searchParams;
-     const email = queryParams.get('email'); // Extract the email from the query parameters
-     const user = await UserSchema.findOne({ email });
+  await connectMongoDB();
+  const queryParams = new URL(request.url).searchParams;
+  const email = queryParams.get("email"); // Extract the email from the query parameters
+  const user = await UserSchema.findOne({ email });
 
-     if (user) {
-     return NextResponse.json({ user });
-     } else {
-     return NextResponse.json({ error: "User not found" }, { status: 404 });
-     }
+  if (user) {
+    return NextResponse.json({ user });
+  } else {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 }
 
 export async function PUT(request) {
-    try {
-      const requestData = await request.json();
-  
-      const { _id, ...updateData } = requestData;
-  
-      await connectMongoDB();
-  
-      await UserSchema.findByIdAndUpdate(_id, updateData);
-  
-      return NextResponse.json({ message: "User Updated", data: requestData }, { status: 200 });
-    } catch (error) {
-      console.error("Error updating user:", error);
-      return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    }
+  try {
+    const requestData = await request.json();
+
+    const { _id, ...updateData } = requestData;
+
+    await connectMongoDB();
+
+    await UserSchema.findByIdAndUpdate(_id, updateData);
+
+    return NextResponse.json(
+      { message: "User Updated", data: requestData },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
+}
